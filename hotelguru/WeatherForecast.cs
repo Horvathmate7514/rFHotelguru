aa770d931879f4396d8787d3ad6f564e57b04db1
makoslecso2004 <kissbarnus04@gmail.com>
2026-03-01 20:23:09 +0100

    test

--- hotelguru/WeatherForecast.cs
+++ hotelguru/WeatherForecast.cs
@@ -2,12 +2,13 @@ namespace hotelguru
 {
     public class WeatherForecast
     {
+        //Cucc
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        //Cucc2
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; set; } //Cucc3
     }
 }
